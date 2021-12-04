ff29c54436d32ca9319d14f4d31b78845af2e9ea
Elinnoel Nunez <elinnoelnunez@outlook.com>
2021-12-03 19:08:27 -0600

    finished day3 part2 ... really bad code but it works...

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -13,7 +14,7 @@ void readFile(string filename, int one[], int zero[], int index) {
     {
         getline(mf, line);
 
-        cout << "line: " << line << endl;
+        // cout << "line: " << line << endl;
 
         if (line == "")
         {
@@ -30,7 +31,7 @@ void readFile(string filename, int one[], int zero[], int index) {
     mf.close();
 }
 
-void outputFun(string filename, string of, char c, int index)
+void outputFun(string filename, string of, char c, int index, vector<int>& bag)
 {
     ofstream outfile(of);
 
@@ -38,32 +39,35 @@ void outputFun(string filename, string of, char c, int index)
 
     ifstream mf(filename);
 
+    int count = 0;
+
+    string prev;
+
     while (!mf.eof())
     {
         getline(mf, line);
 
-        cout << "line: " << line << endl;
+        // cout << "line: " << line << endl;
 
         if (line == "")
         {
             continue;
         }
 
-        // for(int i = 0; i < line.length(); i++) {
-        //     if(line[i] == '1') {
-        //         oneArr[i]+=1;
-        //     } else {
-        //         zeroArr[i]+=1;
-        //     }
-        // }
-
         if(line[index] == c) {
             outfile << line << endl;
+            count+=1;
+            prev = line;
         }
     }
 
     outfile.close();
     mf.close();
+
+    if(count == 1) {
+        cout << "final: " << prev << endl;
+        bag.push_back(std::stoi(prev, nullptr, 2));
+    }
 
     // return mf;
 }
@@ -71,7 +75,7 @@ void outputFun(string filename, string of, char c, int index)
 int main()
 {
 
-    ifstream myfile("test3.txt");
+    ifstream myfile("input3.txt");
 
     if (!myfile.is_open())
     {
@@ -101,14 +105,6 @@ int main()
             continue;
         }
 
-        // for(int i = 0; i < line.length(); i++) {
-        //     if(line[i] == '1') {
-        //         oneArr[i]+=1;
-        //     } else {
-        //         zeroArr[i]+=1;
-        //     }
-        // }
-
         if (line[0] == '1')
         {
             oneArr[0] += 1;
@@ -119,68 +115,62 @@ int main()
         }
     }
 
+    vector<int> bag;
+
+    int oneArr2[12];
+    int zeroArr2[12];
+
+    oneArr2[0] = oneArr[0];
+    zeroArr2[0] = zeroArr[0];
+
+    for(int i = 1; i < 12; i++) {
+        oneArr2[i] = 0;
+        zeroArr2[i] = 0;
+    }
+
 
     myfile.close();
 
     if(oneArr[0] >= zeroArr[0]) {
-        outputFun("test3.txt", "filter" + to_string(1) + ".txt", '1', 0);
+        outputFun("input3.txt", "filter" + to_string(1) + ".txt", '1', 0, bag);
+        outputFun("input3.txt", "f" + to_string(1) + ".txt", '0', 0, bag);
     } else {
-        outputFun("test3.txt", "filter" + to_string(1) + ".txt", '0', 0);
+        outputFun("input3.txt", "filter" + to_string(1) + ".txt", '0', 0, bag);
+        outputFun("input3.txt", "f" + to_string(1) + ".txt", '1', 0, bag);
     }
 
-    for(int i = 1; i < 5; i++) {
+
+    for(int i = 1; i < 12; i++) {
         readFile("filter"+ to_string(i) +".txt", oneArr, zeroArr, i);
 
         if(oneArr[i] >= zeroArr[i]) {
-            outputFun("filter"+ to_string(i) + ".txt", "filter" + to_string(i+1) + ".txt", '1', i);
+            outputFun("filter"+ to_string(i) + ".txt", "filter" + to_string(i+1) + ".txt", '1', i, bag);
         } else {
-            outputFun("filter"+ to_string(i) + ".txt", "filter" + to_string(i+1) + ".txt", '0', i);
+            outputFun("filter"+ to_string(i) + ".txt", "filter" + to_string(i+1) + ".txt", '0', i, bag);
         }
     }
 
+    for(int i = 1; i < 12; i++) {
+        readFile("f" + to_string(i) + ".txt", oneArr2, zeroArr2, i);
+
+        if(oneArr2[i] >= zeroArr2[i]) {
+            outputFun("f"+ to_string(i) + ".txt", "f" + to_string(i+1) + ".txt", '0', i, bag);
+        } else {
+            outputFun("f"+ to_string(i) + ".txt", "f" + to_string(i+1) + ".txt", '1', i, bag);
+        }
+    }
+
+    int final = 1;
+
+    for(int i = 0; i < bag.size(); i++) {
+        final *= bag[i];
+        cout << "bag: " << bag[i] << endl;
+    }
 
 
-    // string bingamma;
-    // string binepsilon;
-
-    // for (int i = 0; i < 12; i++)
-    // {
-    //     if (oneArr[i] > zeroArr[i])
-    //     {
-    //         bingamma += "1";
-    //         binepsilon += "0";
-    //     }
-    //     else
-    //     {
-    //         bingamma += "0";
-    //         binepsilon += "1";
-    //     }
-    // }
-
-    // ofstream outfile("pt1output3.txt");
-
-    // int decgamma = std::stoi(bingamma, nullptr, 2);
-    // int decepsilon = std::stoi(binepsilon, nullptr, 2);
-
-    // int total = decgamma * decepsilon;
-
-    // outfile << total << endl;
-
-    // cout << bingamma << endl;
-    // cout << binepsilon << endl;
-
-    // cout << "oneArr: " << endl;
-    // for(int i = 0; i < 12; i++) {
-    //     cout << oneArr[i] << endl;
-    // }
-
-    // cout << endl;
-    // cout << "zeroArr: " << endl;
-    // for (int i = 0; i < 12; i++) {
-    //     cout << zeroArr[i] << endl;
-    // }
-
-    // outfile.close();
+    ofstream outfile("p2out3.txt");
+    cout << "Final: " << final << endl;
+    outfile << final << endl;
 
     return 0;
 }
